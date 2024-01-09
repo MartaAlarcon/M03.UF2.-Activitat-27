e840e09d2ef9b2dd888f5b9540651b6f761e6c12
MartaAlarcon <marta.alarcon.7e7@itb.cat>
2024-01-09 13:52:17 +0100

    Add project files.

---
+++ M03.UF2. Activitat 27/Program.cs
@@ -0,0 +1,52 @@
+﻿using System; 
+namespace Activitats
+{
+    public class ej27
+    {
+        public static void Main()
+        {
+            int num;
+            int count = 0;
+            Console.WriteLine("Escriu un nombre: ");
+            num = Convert.ToInt32 (Console.ReadLine());
+            int i = num;
+            Console.WriteLine(DeterminarPrimRec(num, count, i));
+        }
+        public static bool DeterminarPrimIt (int num)
+        {
+            int count = 0;
+            for (int i = num; i > 0; i--)
+            {
+                if (num%i == 0)
+                {
+                    count++;
+                }
+            }
+            if (count == 2)
+            {
+                return true;
+            }
+            else return false;
+        }
+        public static bool DeterminarPrimRec (int num, int count, int i)
+        {
+            if (i == 0)
+            {
+                if (count == 2)
+                {
+                    return true;
+                }
+                else return false;
+            }
+            else
+            {
+                if ((num % i) == 0)
+                {
+                    count++;
+                }
+                return DeterminarPrimRec(num, count, i -1);
+
+            }
+        }
+    }
+}
